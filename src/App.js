11f78778354a3e49f243929dbf1f15c87fe52a11
krzysiek <krzysztof@like.agency>
2019-08-11 19:40:42 +0200

    Add state and state handler

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import Header from './Components/Header/Header';
 import Form from './Components/Form/Form';
 
 class App extends Component {
+  state = {
+    weatherData: '',
+  };
+
+  handleState = data => {
+    this.setState({
+      weatherData: data,
+    });
+  };
+
   render() {
+    console.log(this.state.weatherData);
+
     return (
       <>
         <Header />
         <div className="container">
-          <Form />
+          <Form handleState={this.handleState} />
         </div>
       </>
     );
