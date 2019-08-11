3993a8a0c8bb59998b9983aa91b206d5982959dc
krzysiek <krzysztof@like.agency>
2019-08-11 02:19:37 +0200

    Add styles in to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('app-container'));
 
