f24a0d6e9567e2319bc7fba1bc53aa9db5db2630
krzysiek <krzysztof@like.agency>
2019-08-14 23:36:16 +0200

    Remove ApiRequest component

--- src/Components/ApiRequest/ApiRequest.js
+++
@@ -1,7 +0,0 @@
-const ApiRquest = location => {
-  const data = `${process.env.REACT_APP_WEATHER_API_URL}${location}${process.env.REACT_APP_WEATHER_API_KEY}`;
-
-  return data;
-};
-
-export default ApiRquest;
