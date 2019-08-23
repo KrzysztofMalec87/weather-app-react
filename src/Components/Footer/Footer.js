d3ea95449ed1bf38e2aa8a3dd0e2ea4991b4ee14
krzysiek <krzysztof@like.agency>
2019-08-23 23:49:01 +0200

    Beautify js and styles

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,11 @@ import { FadeInTop } from '../../common/Animations/Animations';
 
 const Footer = ({ stateAnimation }) => {
   return (
-    <FadeInTop animationDelay={400} className="footer" pose={stateAnimation ? 'visible' : 'hidden'}>
+    <FadeInTop
+      animationDelay={400}
+      className="footer"
+      pose={stateAnimation ? 'visible' : 'hidden'}
+    >
       <footer className="container">
         App by Krzysztof Malec | www.krzysztofmalec.com
       </footer>
