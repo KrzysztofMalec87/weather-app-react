81a338407c79b9ada5655ee8e915ee3f9172d693
krzysiek <krzysztof@like.agency>
2019-08-24 12:11:41 +0200

    Rebuild Weather icon component

--- src/Components/WeatherIcon/WeatherIcon.js
+++ src/Components/WeatherIcon/WeatherIcon.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const WeatherIcon = ({ icon, description }) => {
+const WeatherIcon = ({ icon, description, additionalClass }) => {
   const imageLink = require(`../../assets/images/icons/${icon}.png`);
+  const iconClass = additionalClass
+    ? `weather-image ${additionalClass}`
+    : 'weather-image';
 
-  return <img alt={description} className="weather-image" src={imageLink} />;
+  return <img alt={description} className={iconClass} src={imageLink} />;
 };
 
 export default WeatherIcon;
