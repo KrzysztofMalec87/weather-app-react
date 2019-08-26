5cc35cd3670828c78e089fd6f8c3cefc832b5969
krzysiek <krzysztof@like.agency>
2019-08-26 20:44:31 +0200

    Remove unnecessary return on Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,18 +3,16 @@ import React from 'react';
 import logo from '../../assets/images/logo.png';
 import { FadeInTop } from '../../common/Animations/Animations';
 
-const Header = ({ animationState }) => {
-  return (
-    <FadeInTop className="header" pose={animationState ? 'visible' : 'hidden'}>
-      <FadeInTop
-        animationDelay={300}
-        className="container header__container"
-        pose={animationState ? 'visible' : 'hidden'}
-      >
-        <img alt="Weather App Logo" className="header__logo" src={logo} />
-      </FadeInTop>
+const Header = ({ animationState }) => (
+  <FadeInTop className="header" pose={animationState ? 'visible' : 'hidden'}>
+    <FadeInTop
+      animationDelay={300}
+      className="container header__container"
+      pose={animationState ? 'visible' : 'hidden'}
+    >
+      <img alt="Weather App Logo" className="header__logo" src={logo} />
     </FadeInTop>
-  );
-};
+  </FadeInTop>
+);
 
 export default Header;
