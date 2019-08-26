6fe61b1dcd309c26102eee9450a60db84d3621d0
krzysiek <krzysztof@like.agency>
2019-08-26 20:43:36 +0200

    Remove unnecessary return

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,18 +2,15 @@ import React from 'react';
 
 import { FadeInTop } from '../../common/Animations/Animations';
 
-const Footer = ({ animationState }) => {
-  return (
-    <FadeInTop
-      animationDelay={400}
-      className="footer"
-      pose={animationState ? 'visible' : 'hidden'}
-    >
-      <footer className="container">
-        App by Krzysztof Malec | www.krzysztofmalec.com
-      </footer>
-    </FadeInTop>
-  );
-};
-
+const Footer = ({ animationState }) => (
+  <FadeInTop
+    animationDelay={400}
+    className="footer"
+    pose={animationState ? 'visible' : 'hidden'}
+  >
+    <footer className="container">
+      App by Krzysztof Malec | www.krzysztofmalec.com
+    </footer>
+  </FadeInTop>
+);
 export default Footer;
