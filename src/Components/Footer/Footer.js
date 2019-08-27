5acd95543f8bb0138ba5e0e7a4a48d6c68931fce
krzysiek <krzysztof@like.agency>
2019-08-27 19:38:29 +0200

    Change footer and header components props name

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import { FadeInTop } from '../../common/Animations/Animations';
 
-const Footer = ({ stateAnimation }) => (
+const Footer = ({ animationState }) => (
   <FadeInTop
     animationDelay={400}
     className="footer"
-    pose={stateAnimation ? 'visible' : 'hidden'}
+    pose={animationState ? 'visible' : 'hidden'}
   >
     <footer className="container">
       App by Krzysztof Malec | www.krzysztofmalec.com
