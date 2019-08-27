0c068fdb535357705494eea6de813a9c2382fa13
krzysiek <krzysztof@like.agency>
2019-08-27 19:31:21 +0200

    Change footer js syntax

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,18 +2,16 @@ import React from 'react';
 
 import { FadeInTop } from '../../common/Animations/Animations';
 
-const Footer = ({ stateAnimation }) => {
-  return (
-    <FadeInTop
-      animationDelay={400}
-      className="footer"
-      pose={stateAnimation ? 'visible' : 'hidden'}
-    >
-      <footer className="container">
-        App by Krzysztof Malec | www.krzysztofmalec.com
-      </footer>
-    </FadeInTop>
-  );
-};
+const Footer = ({ stateAnimation }) => (
+  <FadeInTop
+    animationDelay={400}
+    className="footer"
+    pose={stateAnimation ? 'visible' : 'hidden'}
+  >
+    <footer className="container">
+      App by Krzysztof Malec | www.krzysztofmalec.com
+    </footer>
+  </FadeInTop>
+);
 
 export default Footer;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,18 +3,16 @@ import React from 'react';
 import logo from '../../assets/images/logo.png';
 import { FadeInTop } from '../../common/Animations/Animations';
 
-const Header = ({ stateAnimation }) => {
-  return (
-    <FadeInTop className="header" pose={stateAnimation ? 'visible' : 'hidden'}>
-      <FadeInTop
-        animationDelay={300}
-        className="container header__container"
-        pose={stateAnimation ? 'visible' : 'hidden'}
-      >
-        <img alt="Weather App Logo" className="header__logo" src={logo} />
-      </FadeInTop>
+const Header = ({ stateAnimation }) => (
+  <FadeInTop className="header" pose={stateAnimation ? 'visible' : 'hidden'}>
+    <FadeInTop
+      animationDelay={300}
+      className="container header__container"
+      pose={stateAnimation ? 'visible' : 'hidden'}
+    >
+      <img alt="Weather App Logo" className="header__logo" src={logo} />
     </FadeInTop>
-  );
-};
+  </FadeInTop>
+);
 
 export default Header;
