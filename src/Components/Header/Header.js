315aea6e7ff4780002da6687d586ff2e5efdb36d
krzysiek <krzysztof@like.agency>
2019-08-27 19:40:55 +0200

    Change footer and header components props name

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,12 +3,12 @@ import React from 'react';
 import logo from '../../assets/images/logo.png';
 import { FadeInTop } from '../../common/Animations/Animations';
 
-const Header = ({ stateAnimation }) => (
-  <FadeInTop className="header" pose={stateAnimation ? 'visible' : 'hidden'}>
+const Header = ({ animationState }) => (
+  <FadeInTop className="header" pose={animationState ? 'visible' : 'hidden'}>
     <FadeInTop
       animationDelay={300}
       className="container header__container"
-      pose={stateAnimation ? 'visible' : 'hidden'}
+      pose={animationState ? 'visible' : 'hidden'}
     >
       <img alt="Weather App Logo" className="header__logo" src={logo} />
     </FadeInTop>
